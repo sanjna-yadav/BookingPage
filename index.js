3c50da797060e784a589c63799053dabe17eab2f
sanjana yadav <sanjanayadav2705@gmail.com>
2022-01-29 23:35:34 +0530

    trial1

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.post('/tour', async (req, res,next) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log('on port 3000!!')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
+
